f7fb4de80e90f7ec4fb54f0e05aecf1022ee495e
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2018-10-25 07:21:51 -0700

    tweak reset ports in mon/cim subsystem

--- cim_12x.v
+++ cim_12x.v
@@ -2,9 +2,13 @@
 
 // Cascaded Integrator Multiplexor
 module cim_12x #(
-	parameter dw=32  // data width of mon_chan output
+	parameter dw=32,  // data width of mon_chan output
 	// should be CIC input data width (18), plus 2 * log2(max sample period)
 	// also should match width of sr_out port
+	parameter scale = 18'd65536  // applies to non-downconverted channels
+	// (inm and outm), opportunity to use values that trigger synthesis
+	// of an actual multiplier to get scaling consistency with
+	// downconverted channels, e.g., 18'd61624 == floor((32/33)^2*2^16)
 ) (
 	input clk,
 	input signed [15:0] adca,
@@ -22,7 +26,8 @@ module cim_12x #(
 
 	// unprocessed double-integrator output
 	output [dw-1:0] sr_out,
-	output sr_val
+	output sr_val,
+	input reset
 );
 
 `ifdef SIMULATE
@@ -31,16 +36,15 @@ module cim_12x #(
 `define FILL_BIT 1'b0
 `endif
 
-wire [17:0] scale = 18'd61624;  // floor((32/33)^2*2^16)
 // Each mon_2chan instantiation includes (twice, one for cos, one for sin) the multiplier, double integrator, and sampling/shift-out register
 // Snapshots double-integrator outputs at times flagged by "sample", then shifts the results out on the next twelve cycles.
 wire signed [dw-1:0] s01;  wire g01;
-wire signed [dw-1:0] s03;  wire g03;  mon_2chan #(.dwi(16), .rwi(dw)) mon01(.clk(clk), .adcf(adca), .mcos(cosa), .msin(sina), .samp(sample), .s_in(s03), .s_out(s01), .g_in(g03), .g_out(g01));
-wire signed [dw-1:0] s05;  wire g05;  mon_2chan #(.dwi(16), .rwi(dw)) mon03(.clk(clk), .adcf(adcb), .mcos(cosa), .msin(sina), .samp(sample), .s_in(s05), .s_out(s03), .g_in(g05), .g_out(g03));
-wire signed [dw-1:0] s07;  wire g07;  mon_2chan #(.dwi(16), .rwi(dw)) mon05(.clk(clk), .adcf(adcc), .mcos(cosa), .msin(sina), .samp(sample), .s_in(s07), .s_out(s05), .g_in(g07), .g_out(g05));
-wire signed [dw-1:0] s09;  wire g09;  mon_2chiq #(.dwi(16), .rwi(dw)) mon07(.clk(clk), .iqd(inm),  .scale(scale), .iqs(iqs),  .samp(sample), .s_in(s09), .s_out(s07), .g_in(g09), .g_out(g07));
-wire signed [dw-1:0] s11;  wire g11;  mon_2chiq #(.dwi(16), .rwi(dw)) mon09(.clk(clk), .iqd(outm), .scale(scale), .iqs(iqs),  .samp(sample), .s_in(s11), .s_out(s09), .g_in(g11), .g_out(g09));
-wire signed [dw-1:0] s13;  wire g13;  mon_2chan #(.dwi(16), .rwi(dw)) mon11(.clk(clk), .adcf(adcx), .mcos(cosb), .msin(sinb), .samp(sample), .s_in(s13), .s_out(s11), .g_in(g13), .g_out(g11));
+wire signed [dw-1:0] s03;  wire g03;  mon_2chan_reset #(.dwi(16), .rwi(dw)) mon01(.clk(clk), .adcf(adca), .mcos(cosa), .msin(sina), .samp(sample), .s_in(s03), .s_out(s01), .g_in(g03), .g_out(g01), .reset(reset));
+wire signed [dw-1:0] s05;  wire g05;  mon_2chan_reset #(.dwi(16), .rwi(dw)) mon03(.clk(clk), .adcf(adcb), .mcos(cosa), .msin(sina), .samp(sample), .s_in(s05), .s_out(s03), .g_in(g05), .g_out(g03), .reset(reset));
+wire signed [dw-1:0] s07;  wire g07;  mon_2chan_reset #(.dwi(16), .rwi(dw)) mon05(.clk(clk), .adcf(adcc), .mcos(cosa), .msin(sina), .samp(sample), .s_in(s07), .s_out(s05), .g_in(g07), .g_out(g05), .reset(reset));
+wire signed [dw-1:0] s09;  wire g09;  mon_2chiq_reset #(.dwi(16), .rwi(dw)) mon07(.clk(clk), .iqd(inm),  .scale(scale), .iqs(iqs),  .samp(sample), .s_in(s09), .s_out(s07), .g_in(g09), .g_out(g07), .reset(reset));
+wire signed [dw-1:0] s11;  wire g11;  mon_2chiq_reset #(.dwi(16), .rwi(dw)) mon09(.clk(clk), .iqd(outm), .scale(scale), .iqs(iqs),  .samp(sample), .s_in(s11), .s_out(s09), .g_in(g11), .g_out(g09), .reset(reset));
+wire signed [dw-1:0] s13;  wire g13;  mon_2chan_reset #(.dwi(16), .rwi(dw)) mon11(.clk(clk), .adcf(adcx), .mcos(cosb), .msin(sinb), .samp(sample), .s_in(s13), .s_out(s11), .g_in(g13), .g_out(g11), .reset(reset));
 
 // terminate the chain
 assign s13 = {dw{`FILL_BIT}};
